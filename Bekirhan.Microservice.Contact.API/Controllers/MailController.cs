37c3894c16067a5369913377ad274959fc1406a0
Bekir <Bekir@BEKIRHANPC>
2021-06-11 16:31:43 +0300

    url edit

--- Bekirhan.Microservice.Contact.API/Controllers/MailController.cs
+++ Bekirhan.Microservice.Contact.API/Controllers/MailController.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace Bekirhan.Microservice.Contact.API.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route("[controller]/[action]")]
     [ApiController]
     public class MailController : Controller
     {
