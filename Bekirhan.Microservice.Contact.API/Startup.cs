bb872d4899e71750574047b266867b5f0d07dd19
Bekir <Bekir@BEKIRHANPC>
2021-06-11 16:27:01 +0300

    swagger added

--- Bekirhan.Microservice.Contact.API/Startup.cs
+++ Bekirhan.Microservice.Contact.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,11 +31,29 @@ namespace Bekirhan.Microservice.Contact.API
         {
             services.AddControllers(); 
             Business.ServiceConfiguration.DependencyConfiguration.ConfigureServices(services);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mail API", Version = "v1" });
+                c.DocInclusionPredicate((name, api) => api.HttpMethod != null);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mail API");
+
+                });
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
